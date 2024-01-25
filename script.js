956277fca491ece1bf98333870d4051c227b28e0
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2024-01-25 16:55:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+
+ let output =document.getElementById("output");
+let form = document.getElementById("btn");
+  let msgi =document.getElementById("text");
+let delayi =document.getElementById("delay");
+form.addEventListener("click",(e)=>{
+	e.preventDefault();
+ const	msg= msgi.value;
+	const delay=delayi.value;
+	if(msg && delay>0)
+	{
+		main(delay,msg);
+	}
+	
+	
+})
+  
+async function main( delay,msg){
+
+	  await new Promise((resolve)=>{
+		  setTimeout(()=>{
+			  resolve();
+		  },delay);
+	  })
+	 output.innerText = msg; 
+}
